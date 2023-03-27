0224172ed27dcef69b0c33c05d03f5399a3c1249
dong2810 <dongvip99@gmail.com>
2023-03-27 09:34:59 +0700

    first

---
+++ MovieAppTest/MovieAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppTestApp.swift
+//  MovieAppTest
+//
+//  Created by QuyNM on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieAppTest/Utils.swift
@@ -0,0 +1,24 @@
+//
+//  Utils.swift
+//  MovieAppTest
+//
+//  Created by QuyNM on 3/24/23.
+//
+
+import Foundation
+
+class Utils {
+    
+    static let jsonDecoder: JSONDecoder = {
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        jsonDecoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return jsonDecoder
+    }()
+    
+    static let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-mm-dd"
+        return dateFormatter
+    }()
+}
